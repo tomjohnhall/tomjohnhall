29c84b2f2c8854b4f2fdf3209c0a9ef36c0e5b34
TJH <thomasjohnhall@gmail.com>
2017-11-22 14:19:38 +0000

    quick fix for gone saxonhouse.co

--- developer/urls.py
+++ developer/urls.py
@@ -6,6 +6,7 @@ from . import views
 app_name = 'developer'
 urlpatterns = [
     url(r'^$', views.developer_index, name="developer_index"),
+    url(r'^detail/(?P<project_id>[0-9]+)/$', views.project, name='project'),
     ]
 
 urlpatterns += staticfiles_urlpatterns()
--- developer/views.py
+++ developer/views.py
@@ -6,3 +6,20 @@ from models import Project
 def developer_index(request):
     projects = Project.objects.all()
     return render(request, 'developer.html', {'projects': projects})
+
+def project(request, project_id):
+    project = Project.objects.get(id=project_id)
+    prev_id = project.id - 1
+    next_id = project.id + 1
+    # try for a previous/next transaction and convert to string
+    try:
+        Project.objects.get(id = prev_id)
+        prev_project = str(prev_id)
+    except Project.DoesNotExist:
+        prev_project = None
+    try:
+        Project.objects.get(id = next_id)
+        next_project = str(next_id)
+    except Project.DoesNotExist:
+        next_project = None
+    return render(request, 'project.html', {'project': project, 'prev_project': prev_project, 'next_project': next_project})
